e057a30b2b16d4f325c1c408d218580d2d592ff4
softcp226 <105775851+softcp226@users.noreply.github.com>
2023-07-15 00:15:51 +0100

    Update check_login.js

--- javascript/check_login.js
+++ javascript/check_login.js
@@ -12,7 +12,7 @@ const look_for_user = (cname) => {
     }
   }
   // return "";
-  window.location.href = "/login.html";
+  window.location.replace("signin.html");
 };
 
 // let user_is_active = look_for_user("is_active");
@@ -35,5 +35,5 @@ const look_for_user = (cname) => {
     }
   }
   // return "";
-  window.location.href = "/login.html";
+  window.location.replace("signin.html")
 })("token");
